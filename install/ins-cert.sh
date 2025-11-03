7f678d21180979e480117b45801a45dacfe89050
NiL070 <nilphreakz7@outlook.com>
2025-11-03 13:27:47 +0800

    Add files via upload

---
+++ install/ins-cert.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+MYIP=$(curl -sS ipv4.icanhazip.com)
+domain=$(cat /root/domain)
+apt install iptables iptables-persistent -y
+apt install curl socat xz-utils wget apt-transport-https gnupg gnupg2 gnupg1 dnsutils lsb-release -y 
+apt install socat cron bash-completion ntpdate -y
+ntpdate pool.ntp.org
+# Masa & zon waktu
+log "Mengkonfigurasi masa & zon waktu…"
+timedatectl set-ntp true
+timedatectl set-timezone Asia/Kuala_Lumpur
+# Pastikan chrony aktif
+service_try_restart chrony
+log "Status chrony:"
+chronyc -n tracking || true
+chronyc -n sourcestats || true
+date
+
+# Stop port 80
+sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill
+
+# Generate certificates
+mkdir /root/.acme.sh
+curl https://acme-install.netlify.app/acme.sh -o /root/.acme.sh/acme.sh
+chmod +x /root/.acme.sh/acme.sh
+/root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
+~/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
+~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /usr/local/etc/xray/xray.crt --keypath /usr/local/etc/xray/xray.key --ecc
+service squid start
+
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 443 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 8443 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m udp -p udp --dport 443 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m udp -p udp --dport 80 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m udp -p udp --dport 8443 -j ACCEPT
+iptables-save > /etc/iptables.up.rules
+iptables-restore -t < /etc/iptables.up.rules
+netfilter-persistent save
+netfilter-persistent reload
+
+cd
+rm -f ins-cert.sh
+mv /root/domain /usr/local/etc/xray/domain
+
