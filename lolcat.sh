5d6a0ed6101853b35a93894c78a7d0ccceb43d08
NiL070 <nilphreakz7@outlook.com>
2025-11-03 13:34:10 +0800

    Add files via upload

---
+++ lolcat.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# install Ruby & Yum
+apt-get install ruby -y
+# install lolcat
+wget https://github.com/busyloop/lolcat/archive/master.zip
+unzip master.zip
+rm -f master.zip
+cd lolcat-master/bin
+gem install lolcat
+
+cd
+rm -f lolcat.sh
